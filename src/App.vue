18547eff2008017b06187f53be3be6bf4dab79a8
Fernando <farriagadal94@gmail.com>
2023-07-11 17:29:42 -0400

    style: se cambio estilo de fuentes

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,11 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap');
 
-#app {
+* {
   font-family: 'DM Serif Display', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 body {
