295e22c8d826b0ae4e5b27a8e9c3b8a4aa64fc8d
Fernando <farriagadal94@gmail.com>
2023-07-10 21:33:39 -0400

    feat: se agrego HoteList

---
+++ src/components/HotelList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hotel-list">
+    <HotelCard
+      v-for="hotel in hotels"
+      :key="hotel.name"
+      :hotel="hotel"
+      @select="$emit('select', $event)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HotelCard from './HotelCard.vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  name: 'HotelList',
+  components: {
+    HotelCard
+  },
+  setup () {
+    const store = useStore()
+    const hotels = store.state.hotel.hotels // Obteniendo los hoteles desde el store de Vuex
+    console.log(hotels)
+
+    return {
+      hotels
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.hotel-list {
+  display: flex;
+  margin: auto;
+  width: fit-content;
+  gap: 40px;
+  margin-top: 70px;
+}
+</style>
